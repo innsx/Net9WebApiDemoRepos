c93586515214729981c95abae8c7cb38307810db
innsx <148797793+innsx@users.noreply.github.com>
2025-08-15 16:58:08 -0400

    Update User.cs

--- Net9WebApi/Models/User.cs
+++ Net9WebApi/Models/User.cs
@@ -7,17 +7,10 @@
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
-
-
-namespace Net9WebApi.Models
-{
-    public class User
-    {
-
-      public string Address {get; set;} = string.Empty;
-      public string City {get; set;} = string.Empty;
-      public string State {get; set;} = string.Empty;
-      public string ZipCode {get; set;} = string.Empty;
-
+        public string Address {get; set;} = string.Empty;
+        public string City {get; set;} = string.Empty;
+        public string State {get; set;} = string.Empty;
+        public string ZipCode {get; set;} = string.Empty;
     }
+
 }
