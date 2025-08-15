e89c2b330418c0e9e56b6ca3ab73314c65397b19
Nick Xiong <kou20.xiong30@gmail.com>
2025-08-15 17:03:15 -0400

    3rd commits - changes Models/User

--- Net9WebApi/Models/User.cs
+++ Net9WebApi/Models/User.cs
@@ -4,8 +4,8 @@
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
+        public string Name { get; set; } = "Jon Doe";
+        public string Email { get; set; } = "jd@hotmail.com";
         public string Password { get; set; } = string.Empty;
         public string Address {get; set;} = string.Empty;
         public string City {get; set;} = string.Empty;
