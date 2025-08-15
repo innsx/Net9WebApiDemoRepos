213e31b74385f0c11151a2afc02562693326644f
innsx <148797793+innsx@users.noreply.github.com>
2025-08-15 16:50:42 -0400

    Update User.cs

--- Net9WebApi/Models/User.cs
+++ Net9WebApi/Models/User.cs
@@ -1,1 +1,12 @@
 
+namespace Net9WebApi.Models
+{
+    public class User
+    {
+      public string Name {get; set; };
+      public string Address {get; set;} = string.Empty;
+      public string City {get; set;} = string.Empty;
+      public string State {get; set;} = string.Empty;
+      public string ZipCode {get; set;} = string.Empty;
+    }
+}
